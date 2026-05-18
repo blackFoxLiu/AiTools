eb5f608c29451bc23aed08914ca82935b66e296a
blackFoxLiu <1959966704@qq.com>
2026-05-18 22:35:29 +0800

    医疗诊断项目-配置文件

---
+++ 医疗Langgraph实验_aaaa/config/hit_config.py
@@ -0,0 +1,8 @@
+class Config:
+    COLLECTION_NAME = "search_hit"
+    PERSIST_DIRCTORY = "D:/PythonProjects/AiTools/医疗Langgraph实验_aaaa/data/databases/search_hit"
+    # 疾病更新置信度阈值
+    PROBABILITY_THRESHOLD = 70
+
+
+config = Config()
